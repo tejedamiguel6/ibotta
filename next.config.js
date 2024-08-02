8269cd0ccdc222b88a36bbdf1afb9b6ee15e143b
Miguel Tejeda <tejedamiguel6@gmail.com>
2024-08-01 18:50:07 -0600

    added new domain for image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['images.ctfassets.net', 'i.scdn.co'],
+    domains: ['images.ctfassets.net', 'i.scdn.co', 'charts-images.scdn.co'],
   },
 }
